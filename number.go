c176d8068592c29a4cddf91aff907920803f09c4
AhmadRendi <ahmadrendiajah@gmail.com>
2025-09-09 22:47:30 +0800

    belajar type data number

--- number.go
+++ number.go
@@ -1,1 +1,17 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga Koma Lime = ", 3.5)
+
+	fmt.Println("Hasil Dari Menambahkan : ", add(10, 20))
+
+}
+
+func add(a int, b int) int {
+	fmt.Println("Add a = ", a, "b = ", b)
+	return a + b
+}
